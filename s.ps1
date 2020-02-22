0da0bf8a1d00e52a2f081688fcf9c97ff9ab407a
Shashi ebbare <syebbare@skydevops.in>
2020-02-22 20:47:37 +0530

    modified the jenkinsfile, backedup

--- s.ps1
+++ s.ps1
@@ -45,3 +45,25 @@ Invoke-RestMethod -Headers $Headers -Method POST -Body $payload -Uri $SDURI
                                                                   Write-Host $sdUri
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12                                                                  
 Invoke-RestMethod -Uri https://api.github.com/repos/sky-kshatriyan/sdmvnclm/statuses/$commitId -Method 'POST' -ContentType 'application/json' -Headers @{Authorization=('Basic {0}' -f $SDToken)} -Body $payload
+
+
+
+// Jenkinsfile
+// Testing PR from MBPLJ
+
+node {
+  stage('pre-build') {
+      checkout scm
+  }
+  stage('Build') {
+    if (isUnix()) {
+      withMaven(jdk: 'java180', maven: 'mvn362') {
+          sh label: 'AEM_Prj', script: 'mvn clean'
+      }
+    }else {
+      withMaven(jdk: 'java180', maven: 'mvn362') {
+        bat label: 'AEM_Prj', script: 'mvn clean'
+      }
+    }
+  }
+}
